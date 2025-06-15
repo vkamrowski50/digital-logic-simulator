1d7a44bc17eafe19e564c0160602da18a488611c
Victor Kamrowski <vkamrowski50@stuy.edu>
2025-06-15 18:15:48 -0400

    tested full adder, works

--- tests/test_combinational.py
+++ tests/test_combinational.py
@@ -1,5 +1,6 @@
 import unittest
 from combinational.half_adder import HalfAdder
+from combinational.full_adder import FullAdder
 
 class TestCombinationals(unittest.TestCase):
     def test_half_adder(self):
@@ -15,3 +16,24 @@ class TestCombinationals(unittest.TestCase):
         g = HalfAdder("HA4")
         g.add_inputs(1,1)
         self.assertEqual(g.compute_outputs(),(0,1))
+    
+    def test_full_adder(self):
+        test_cases = [
+            ((0,0,0), (0,0)),
+            ((0,0,1), (1,0)),
+            ((0,1,0), (1,0)),
+            ((0,1,1), (0,1)),
+            ((1,0,0), (1,0)),
+            ((1,0,1), (0,1)),
+            ((1,1,0), (0,1)),
+            ((1,1,1), (1,1))
+        ]
+
+        for i, (inputs,expected) in enumerate(test_cases):
+            with self.subTest(case=i, input_values=inputs):
+                g = FullAdder(f"FA{i}")
+                g.add_inputs(*inputs)
+                self.assertEqual(g.compute_outputs(),expected)
+
+if __name__ == '__main__':
+    unittest.main()
